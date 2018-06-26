d100ca7538dc582419f03dc739fc5b2317128831
abbas <abbas4u.haider@gmail.com>
2018-06-26 20:22:29 +0530

    folder-panel included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FoldersPanelComponent } from './folders-panel/folders-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoldersPanelComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/folders-panel/folders-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-folders-panel',
+  templateUrl: './folders-panel.component.html',
+  styleUrls: ['./folders-panel.component.css']
+})
+export class FoldersPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
